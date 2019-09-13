0569349c5169e00d38064f6a24aad3fcdf6cd13c
avilaacejohn <avilaacejohn@gmail.com>
2019-09-13 17:52:17 +0800

    Add success alert - ngx-toastr package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from "@angular/common/http"; 
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { EmployeeComponent } from './employees/employee/employee.component';
@@ -19,7 +22,10 @@ import { EmployeeService } from './shared/employee.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
+
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
